91bc9d497e1dbbc61bf0e26cd8c29a708620d30e
Tiago Tunes <126902296+th-tunes@users.noreply.github.com>
2023-04-15 00:36:02 +0000

    add banner rotativo no index

---
+++ js/banner.js
@@ -0,0 +1,27 @@
+let bannerImgs = [];
+
+const banner = document.querySelector('.container-banner-index');
+
+let bannerImgsIndex = 0;
+const changeImageInterval = 5000;
+
+async function startBanner() {
+  const response = await fetch('./json/bannerImage.json');
+  bannerImgs = await response.json();
+
+  onBannerImageChange();
+  setInterval(onBannerImageChange, changeImageInterval);  
+}
+
+function onBannerImageChange() {
+  const img = bannerImgs[bannerImgsIndex];
+  banner.style.backgroundImage = `url(${img})`;
+
+  if(bannerImgsIndex === bannerImgs.length - 1) {
+    bannerImgsIndex = 0;
+  } else {
+    bannerImgsIndex++;    
+  }
+}
+
+startBanner();
